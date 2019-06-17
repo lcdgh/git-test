db16c2975b725a4494fadcbbfb869f8926541936
GameBoy <1411376786@qq.com>
2019-06-17 20:41:04 +0800

    修改添加个注释

--- git_test_project/src/com/itheima/git/FirstClass.java
+++ git_test_project/src/com/itheima/git/FirstClass.java
@@ -7,6 +7,8 @@ public class FirstClass {
         System.out.println("Hello git");
         System.out.println("Hello git");
         System.out.println("Hello git");
+        //添加点注释
+        //再添加个注释
     }
     public void SecondClass(){
         System.out.println("哈~哈~哈");
